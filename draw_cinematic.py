ab31ef89bfededabd3797c6a68c3b9944ffe3f60
XarpeSerpe <xarpeserpe@gmail.com>
2013-08-24 05:03:40 +0200

    Added no_force and lateral_force physics.

--- draw_cinematic.py
+++ draw_cinematic.py
@@ -5,58 +5,66 @@
 #Anhadir vectores
 
 
-import sys, pygame, math, random
+import sys, math, random, pygame
+from planetario import *
 from color import *
 from cinematic import *
 from axis import x_axis, y_axis
 
 pygame.init()
-size = width, height = 800 , 450
+size = width, height = 1920 , 1080
 
-screen = pygame.display.set_mode(size,pygame.FULLSCREEN)
-screen.fill(black)
 
-mainloop =  True
-Clock = pygame.time.Clock()
-
-while mainloop:
-    x_axis(screen)
-    y_axis(screen)
-    tickFPS = Clock.tick(35)
-    #pygame.display.set_caption("Press Esc to quit. FPS: %.2f" % (Clock.get_fps()))
-    #pygame.draw.circle(screen, black,[int(x),int(y)] , 5, 0)
-    #fontsize = random.randint(35, 150)
-    #myFont = pygame.font.SysFont("None", fontsize)
-    #color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-    #screen.fill(black)
-    #screen.blit(myFont.render("I love pygame!", 0, (color)), (x,y))
-    #start_pos = random.randint(0,800),random.randint(0,800)
-    #end_pos = random.randint(0,450),random.randint(0,450)
-    #
-    pygame.draw.line(screen, black, [width/2, y], [x,y], 1) 
-    pygame.draw.line(screen, black, [x, height/2], [x,y], 1)  
-
-  
-    t = t +0.1
-    x_old=x
-    y_old=y
-    x = x_0 + v_0x * t
-    y = height -(y_0+v_0y*t+g/2*t**2)
-    
-    if y > height :
-        t = 0
-    if x > width :
-        t = 0
-    pygame.draw.circle(screen, blue,[int(x),int(y)] , 5, 0)
-    pygame.draw.line(screen, red, [x_old, y_old], [x,y], 1) 
-    pygame.draw.line(screen, blue, [width/2, y], [x,y], 1) 
-    pygame.draw.line(screen, blue, [x, height/2], [x,y], 1)  
+def espera ( ):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             mainloop = False # Be IDLE friendly!
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 mainloop = False # Be IDLE friendly!
+                
+screen = pygame.display.set_mode(size)#,pygame.FULLSCREEN)
+screen.fill(black)
+
+mainloop =  True
+
+Clock = pygame.time.Clock()
+
+t = 0
+while mainloop:
+    
+     
+   #draw reference sistem 
+    x_axis(screen)
+    y_axis(screen)
+    
+    tickFPS = Clock.tick(35)
+    #pygame.display.set_caption("Press Esc to quit. FPS: %.2f" % (Clock.get_fps()))
+    #
+    #fontsize = random.randint(35, 150)
+    #myFont = pygame.font.SysFont("None", fontsize)
+    #color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    #screen.fill(black)
+    #screen.blit(myFont.render("I love pygame!", 0, (color)), (x,y))
+    #trayectoria pygame.draw.line(screen, red, [x_old, y_old], [x,y], 1) 
+    # Coordenadas:
+    #Clean axis
+    #pygame.draw.line(screen, black, [width/2, y], [x,y], 1) 
+    #pygame.draw.line(screen, black, [x, height/2], [x,y], 1)  
+    #pygame.draw.line(screen, blue, [width/2, y], [x,y], 1) 
+    #pygame.draw.line(screen, blue, [x, height/2], [x,y], 1)  
+    
+    #no_force(screen)
+    lateral_force(screen,t)
     pygame.display.update()
- 
+    t = t + 1
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                mainloop = False # Be IDLE friendly!
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    mainloop = False # Be IDLE friendly!    pygame.display.update()
+  
+
+pygame.display.quit()
 pygame.quit() # Be IDLE friendly! 
