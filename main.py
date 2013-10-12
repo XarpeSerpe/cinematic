10e80302f747eff7dadc482f81034c67cd640c68
XarpeSerpe <xarpeserpe@gmail.com>
2013-10-12 07:45:11 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+# TO DO
+#comenzar a trabajar por capas
+#recalibrar ejes para que quepa el movimiento 
+#dibujar numeros en los ejes
+#Anhadir vectores
+
+
+import sys, math, random, pygame
+from dynamics import *
+from screen_setup import *
+
+from axis import x_axis, y_axis
+from scenes import *
+pygame.init()
+
+               
+screen = pygame.display.set_mode(size)#,pygame.FULLSCREEN)
+screen.fill(black)
+
+mainloop =  True
+
+Clock = pygame.time.Clock()
+
+i = 0
+
+while mainloop:
+    i = i + 1 
+    if i < 500:
+        scene_1()
+    elif i == 500:
+        scene_2()
+    else:
+	scene_3() 
+    
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                mainloop = False # Be IDLE friendly!
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    mainloop = False # Be IDLE friendly!    pygame.display.update()
+  
+
+pygame.display.quit()
+pygame.quit() # Be IDLE friendly! 
