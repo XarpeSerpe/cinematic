aff184f19efdc7b9887f7aa7e39fa9a37bc1e1d9
XarpeSerpe <xarpeserpe@gmail.com>
2013-10-26 03:09:06 +0200

    Update scenes.py

--- scenes.py
+++ scenes.py
@@ -31,13 +31,55 @@ def scene_2():
         time.sleep(1)
         pygame.display.update()
     
-
-def scene_3():
+def scene_3 ( ):
+    pass
+    
+def scene_4():
     screen.fill(black)
-    
-    fontsize = random.randint(35, 150)
+    fontsize = (50)
     myFont = pygame.font.SysFont("None", fontsize)
-    
-    #screen.fill(black)
-    screen.blit(myFont.render("Scene 3", 0, (red)), (x,y))
+    screen.blit(myFont.render("Give me V_x", 0, (red)), (x,y))
+    print ask(screen, "Name") + " was entered"
     pygame.display.update()
+   
+def get_key():
+       while 1:
+           event = pygame.event.poll()
+           if event.type == KEYDOWN:
+            return event.key
+       else:
+           pass
+
+def display_box(screen, message):
+  "Print a message in a box in the middle of the screen"
+  fontobject = pygame.font.Font(None,18)
+  pygame.draw.rect(screen, (0,0,0),
+                   ((screen.get_width() / 2) - 100,
+                    (screen.get_height() / 2) - 10,
+                    200,20), 0)
+  pygame.draw.rect(screen, (255,255,255),
+                   ((screen.get_width() / 2) - 102,
+                    (screen.get_height() / 2) - 12,
+                    204,24), 1)
+  if len(message) != 0:
+    screen.blit(fontobject.render(message, 1, (255,255,255)),
+                ((screen.get_width() / 2) - 100, (screen.get_height() / 2) - 10))
+  pygame.display.flip()
+
+def ask(screen, question):
+  "ask(screen, question) -> answer"
+  pygame.font.init()
+  current_string = []
+  display_box(screen, question + ": " + string.join(current_string,""))
+  while 1:
+    inkey = get_key()
+    if inkey == K_BACKSPACE:
+      current_string = current_string[0:-1]
+    elif inkey == K_RETURN:
+      break
+    elif inkey == K_MINUS:
+      current_string.append("_")
+    elif inkey <= 127:
+      current_string.append(chr(inkey))
+    display_box(screen, question + ": " + string.join(current_string,""))
+  return string.join(current_string,"")
